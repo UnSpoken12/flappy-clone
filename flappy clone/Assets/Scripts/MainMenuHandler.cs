25bf9eabb29bf1b16d4b22f9a16596bbe10a3b79
UnSpoken12 <59182480+UnSpoken12@users.noreply.github.com>
2023-04-29 18:56:42 -0400

    Imported GUI elements
    And added them to the game

---
+++ flappy clone/Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public void StartButtonPressed()
+    {
+        
+    }
+
+    public void ExitButtonPressed()
+    {
+        Application.Quit();
+    }
+}
