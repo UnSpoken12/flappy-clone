5f884677e21fc1b88181a8a4c51322e4437e0c0f
UnSpoken12 <59182480+UnSpoken12@users.noreply.github.com>
2023-04-30 10:13:43 -0400

    Created a pause button

--- flappy clone/Assets/Scripts/UIHandler.cs
+++ flappy clone/Assets/Scripts/UIHandler.cs
@@ -3,15 +3,25 @@ using UnityEngine;
 public class UIHandler : MonoBehaviour
 {
     [SerializeField] private Spawner spawner;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject pauseButton;
+    private bool isPaused = false;
 
     public void StartButtonPressed()
     {
         spawner.SetSpawning(true);
-        gameObject.SetActive(false);
+        mainMenu.SetActive(false);
+        pauseButton.SetActive(true);
     }
 
     public void ExitButtonPressed()
     {
         Application.Quit();
+    }
+
+    public void PauseButtonPressed()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = (isPaused) ? 0 : 1;
     }
 }
